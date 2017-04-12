cf987ebcb8dbc416ae9ebfe0e4bd19b633afe1bf
brent-shaw <shaw@live.co.za>
2017-04-13 00:15:18 +0200

    Added frequencyColour

--- display.scope5.js
+++ display.scope5.js
@@ -1,1 +1,8 @@
 //Functions for displaying signals
+
+function frequencyColour(f) 
+{
+  var lf = round(log(f)*log(f));
+  var h = round(map(lf, 0, 100, 0, 255));
+  return color('hsl('+h+',100%, 50%)';
+}
