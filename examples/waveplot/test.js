0a47d4da1aaafeb7aaaa56b3dfbf06a9341c0d8a
brent-shaw <shaw@live.co.za>
2017-04-14 00:00:13 +0200

    Update test.js

--- examples/waveplot/test.js
+++ examples/waveplot/test.js
@@ -15,9 +15,9 @@ function setup()
   myPlane = new Plane(0, 0, 1280, 600);
   myPlane.draw();
 
-  waves.push(new Wave(3300, 100, myPlane, 4));
-  waves.push(new Wave(6600, 100, myPlane, 4));
-  waves.push(new Wave(9900, 100, myPlane, 4));
+  waves.push(new Wave(400, 100, myPlane, 4));
+  waves.push(new Wave(800, 100, myPlane, 4));
+  waves.push(new Wave(1200, 100, myPlane, 4));
 
 
   for (x in waves) 
